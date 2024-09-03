817a113b3324f8dac41f7b1186a45b9031b8a691
Calin Jilavu <23583666+c8j@users.noreply.github.com>
2024-09-03 19:27:38 +0200

    Add number guessing app

---
+++ Grundkurs/GuessTheNumber/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace GuessTheNumber;
+
+class Program
+{
+    static void Main()
+    {
+        int number = new Random().Next(0, 100);
+        bool guessed = false;
+        uint counter = 0;
+        while (!guessed)
+        {
+            Console.Write("Insert number guess: ");
+            if (!int.TryParse(Console.ReadLine(), out int guess))
+            {
+                Console.WriteLine("Please insert a valid number.");
+            }
+            else
+            {
+                counter++;
+                if (guess < number)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else if (guess > number)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else
+                {
+                    Console.WriteLine(
+                        $"Correct! Guessed in {counter} {(counter > 1 ? "tries" : "try")}."
+                        );
+                    guessed = true;
+                }
+            }
+        }
+    }
+}
