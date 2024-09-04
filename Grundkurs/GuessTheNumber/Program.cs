e046d9f0aad50cefb3dadd6f59c5ff8be5c456bb
Calin Jilavu <23583666+c8j@users.noreply.github.com>
2024-09-04 10:01:22 +0200

    Make program loop if user wishes so

--- Grundkurs/GuessTheNumber/Program.cs
+++ Grundkurs/GuessTheNumber/Program.cs
@@ -4,33 +4,76 @@ class Program
 {
     static void Main()
     {
-        int number = new Random().Next(0, 100);
-        bool guessed = false;
-        uint counter = 0;
-        while (!guessed)
+        Random rng = new();
+        bool roundEnd;
+        uint counter;
+        bool exitTriggered = false;
+        int number;
+        string? continueAnswer;
+        while (!exitTriggered)
         {
-            Console.Write("Insert number guess: ");
-            if (!int.TryParse(Console.ReadLine(), out int guess))
+            number = rng.Next(1, 101);
+            roundEnd = false;
+            counter = 0;
+            while (!roundEnd)
             {
-                Console.WriteLine("Please insert a valid number.");
-            }
-            else
-            {
-                counter++;
-                if (guess < number)
+                Console.Write("Insert number guess: ");
+                if (!int.TryParse(Console.ReadLine(), out int guess))
                 {
-                    Console.WriteLine("Higher");
-                }
-                else if (guess > number)
-                {
-                    Console.WriteLine("Lower");
+                    Console.WriteLine("Please insert a valid number.");
                 }
                 else
                 {
-                    Console.WriteLine(
-                        $"Correct! Guessed in {counter} {(counter > 1 ? "tries" : "try")}."
-                        );
-                    guessed = true;
+                    counter++;
+                    if (guess < number)
+                    {
+                        Console.WriteLine("Higher");
+                    }
+                    else if (guess > number)
+                    {
+                        Console.WriteLine("Lower");
+                    }
+                    else
+                    {
+                        Console.WriteLine(
+                            $"Correct! Guessed in {counter} {(counter > 1 ? "tries" : "try")}."
+                            );
+                        roundEnd = true;
+                        Console.Write("Go again? (y/n): ");
+                        continueAnswer = Console.ReadLine();
+                        while (
+                            string.IsNullOrEmpty(continueAnswer) ||
+                            (
+                                !continueAnswer.Equals(
+                                "y",
+                                StringComparison.CurrentCultureIgnoreCase
+                                ) &&
+                                !continueAnswer.Equals(
+                                "n",
+                                StringComparison.CurrentCultureIgnoreCase
+                                )
+                            )
+                            )
+                        {
+                            Console.WriteLine("Please insert a valid option.");
+                            Console.Write("Go again? (y/n): ");
+                            continueAnswer = Console.ReadLine();
+                        }
+                        if (continueAnswer.Equals(
+                                "y",
+                                StringComparison.CurrentCultureIgnoreCase
+                                ))
+                        {
+                            //do nothing special.. idk lol
+                        }
+                        else if (continueAnswer.Equals(
+                                "n",
+                                StringComparison.CurrentCultureIgnoreCase
+                                ))
+                        {
+                            exitTriggered = true;
+                        }
+                    }
                 }
             }
         }
