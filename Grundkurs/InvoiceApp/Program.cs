7923c14c1fac28d416c219fa7a47bcd0036e7865
Calin Jilavu <23583666+c8j@users.noreply.github.com>
2024-09-10 12:51:08 +0300

    Add dummy info and test ToString

--- Grundkurs/InvoiceApp/Program.cs
+++ Grundkurs/InvoiceApp/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using InvoiceApp;
+
+//Skapa ett objekt ifrån klassen Invoice
+Invoice invoice = new();
+
+Console.WriteLine(invoice);
+
+/* Fakturans Basinformation  */
+invoice.InvoiceNumber = 1;
+invoice.InvoiceDate = DateTime.Now;
+invoice.PaymentTerms = 30;
+invoice.DueDate = DateTime.Now.AddDays(invoice.PaymentTerms);
+
+/* Kund information */
+invoice.CustomerNumber = 981256;
+invoice.CustomerName = "Bosse Kofot";
+invoice.CustomerAddress = "Rånarvägen 1";
+invoice.CustomerPostalCode = "666 78";
+invoice.CustomerCity = "Härlanda";
+invoice.CustomerReference = "Sickan Karlsson";
+
+Console.WriteLine(invoice);
