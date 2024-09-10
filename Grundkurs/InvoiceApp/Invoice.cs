530338b74aca3b4b139bc29b85c0052ec5f53115
Calin Jilavu <23583666+c8j@users.noreply.github.com>
2024-09-10 11:05:04 +0300

    Initial commit of InvoiceApp; base information for class

---
+++ Grundkurs/InvoiceApp/Invoice.cs
@@ -0,0 +1,20 @@
+﻿namespace InvoiceApp;
+
+public class Invoice
+{
+    /* PROPERTIES */
+    //Base info
+    public int InvoiceNumber { get; set; }
+    public DateTime InvoiceDate { get; set; }
+    public DateTime DueDate { get; set; }
+    public decimal TotalValue { get; set; }
+
+    //Sender(Company) info
+    public string SenderName { get; set; }
+    public string SenderAddress { get; set; }
+    public string SenderPostalCode { get; set; }
+    public string SenderCity { get; set; }
+    public string SenderPhone { get; set; }
+    public string SenderReference { get; set; }
+    public string SenderReferenceEmail { get; set; }
+}
