24aaf096a392fe82fc0fa5a4e64482c0729454b7
[Calin Jilavu] <23583666+c8j@users.noreply.github.com>
2024-09-24 13:47:59 +0200

    Create web app project using template

---
+++ Grundkurs/Westcoast-shop/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Westcoast_shop.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
