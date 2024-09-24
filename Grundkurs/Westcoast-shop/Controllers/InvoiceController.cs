6ed0df733a1ae09ffaa58c2c7ed4111f380234ce
CalinJilavu <23583666+c8j@users.noreply.github.com>
2024-09-24 15:57:08 +0200

    Add invoice controller

---
+++ Grundkurs/Westcoast-shop/Controllers/InvoiceController.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Westcoast_shop.Controllers;
+
+public class InvoiceController : Controller
+{
+    // GET: InvoiceController
+    public ActionResult Index()
+    {
+        return View();
+    }
+
+}
