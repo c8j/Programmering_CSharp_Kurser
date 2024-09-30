545d644748db04dbe26b6622ea8a8b766699ec22
CalinJilavu <23583666+c8j@users.noreply.github.com>
2024-09-30 21:08:15 +0200

    Simplify initialization

--- Grundkurs/WestcoastEShop-FirstTry/Program.cs
+++ Grundkurs/WestcoastEShop-FirstTry/Program.cs
@@ -5,19 +5,18 @@ using System.Text.Encodings.Web;
 var salesOrder = new SalesOrder
 {
     OrderDate = DateTime.Now,
-    OrderId = 23
-};
-
-salesOrder.Customer = new Customer
-{
-    CustomerId = 12,
-    CreatedAt = DateTime.Now,
-    LastBuy = DateTime.Now,
-    FirstName = "John",
-    LastName = "Smith",
-    AddressLine = "Storgatan 8",
-    PostalCode = "123 45",
-    City = "Storstad"
+    OrderId = 23,
+    Customer = new Customer
+    {
+        CustomerId = 12,
+        CreatedAt = DateTime.Now,
+        LastBuy = DateTime.Now,
+        FirstName = "John",
+        LastName = "Smith",
+        AddressLine = "Storgatan 8",
+        PostalCode = "123 45",
+        City = "Storstad"
+    }
 };
 
 var jsonOptions = new JsonSerializerOptions
