8197f2bcb0749d73cadf9d8dc0e2384076a68693
Calin Jilavu <23583666+c8j@users.noreply.github.com>
2024-09-30 14:27:55 +0200

    Create address class

---
+++ Grundkurs/ReadingWritingJsonFiles/Address.cs
@@ -0,0 +1,8 @@
+namespace ReadingWritingJsonFiles;
+
+public class Address
+{
+    public string AddressLine { get; set; } = "";
+    public string PostalCode { get; set; } = "";
+    public string City { get; set; } = "";
+}
