00a2e7b60de60ea65d41bda7f2e0859fae306223
Calin Jilavu <23583666+c8j@users.noreply.github.com>
2024-09-30 14:54:05 +0200

    Test deserialization

--- Grundkurs/ReadingWritingJsonFiles/Program.cs
+++ Grundkurs/ReadingWritingJsonFiles/Program.cs
@@ -19,12 +19,17 @@ Person person = new()
 Console.WriteLine(person);
 var path = $"{Environment.CurrentDirectory}/data/person.json";
 #pragma warning disable CA1869 // Cache and reuse 'JsonSerializerOptions' instances
-var jsonOption = new JsonSerializerOptions
+var jsonOptions = new JsonSerializerOptions
 {
     WriteIndented = true,
     PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
     Encoder = JavaScriptEncoder.UnsafeRelaxedJsonEscaping
 };
 #pragma warning restore CA1869 // Cache and reuse 'JsonSerializerOptions' instances
-var json = JsonSerializer.Serialize(person, jsonOption);
+var json = JsonSerializer.Serialize(person, jsonOptions);
 File.WriteAllText(path, json);
+
+var person2 = new Person();
+var jsonText = File.ReadAllText(path);
+person2 = JsonSerializer.Deserialize<Person>(jsonText, jsonOptions);
+Console.WriteLine(person2);
