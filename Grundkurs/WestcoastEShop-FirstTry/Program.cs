246957a031311098ffde267df025efe358382905
Calin Jilavu <23583666+c8j@users.noreply.github.com>
2024-09-30 15:40:38 +0200

    Serialize salesOrder and write to file

--- Grundkurs/WestcoastEShop-FirstTry/Program.cs
+++ Grundkurs/WestcoastEShop-FirstTry/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Westcoast_EShop.Models;
+using System.Text.Json;
+using System.Text.Encodings.Web;
+
+var salesOrder = new SalesOrder
+{
+    OrderDate = DateTime.Now,
+    OrderId = 23
+};
+
+var jsonOptions = new JsonSerializerOptions
+{
+    WriteIndented = true,
+    PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+    Encoder = JavaScriptEncoder.UnsafeRelaxedJsonEscaping
+};
+
+var path = $"{Environment.CurrentDirectory}/data/salesOrder.json";
+
+var jsonText = JsonSerializer.Serialize(salesOrder, jsonOptions);
+
+File.WriteAllText(path, jsonText);
