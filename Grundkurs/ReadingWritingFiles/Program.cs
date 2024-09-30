005af6062426b151299f492b4598edeffedeecfa
Calin Jilavu <23583666+c8j@users.noreply.github.com>
2024-09-30 13:21:20 +0200

    Use StreamReader instead

--- Grundkurs/ReadingWritingFiles/Program.cs
+++ Grundkurs/ReadingWritingFiles/Program.cs
@@ -10,8 +10,18 @@ string text = File.ReadAllText(path);
 Console.WriteLine(text); */
 
 using StreamWriter sw = new(path);
+
 string message = "Info added " + DateTime.Now;
 sw.WriteLine(message);
+
 message = "Info added again " + DateTime.Now;
 sw.WriteLine(message);
+
 sw.Close();
+
+using StreamReader sr = new(path);
+
+string text = sr.ReadToEnd();
+Console.WriteLine(text);
+
+sr.Close();
