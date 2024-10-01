97cc9efc51ab26a194f5b0843d582b84668391d7
Calin Jilavu <23583666+c8j@users.noreply.github.com>
2024-10-01 14:36:48 +0200

    List order items

--- Grundkurs/Westcoast-eShop/eShop.App/Program.cs
+++ Grundkurs/Westcoast-eShop/eShop.App/Program.cs
@@ -7,6 +7,13 @@ if (orders is not null)
     foreach (SalesOrder order in orders)
     {
         Console.WriteLine(order);
+        if (order.OrderItems is not null)
+        {
+            foreach (OrderItem item in order.OrderItems)
+            {
+                Console.WriteLine(item);
+            }
+        }
     }
 
     Storage.SaveOrdersToFile(orders);
