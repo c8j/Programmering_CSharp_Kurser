03149c21785d996c94449ca5df38bdfd602fd833
Calin Jilavu <23583666+c8j@users.noreply.github.com>
2024-10-01 09:58:52 +0200

    Test sales order with some dummy data

--- Grundkurs/WestcoastEShop-FirstTry/Models/OrderItem.cs
+++ Grundkurs/WestcoastEShop-FirstTry/Models/OrderItem.cs
@@ -4,8 +4,14 @@ public class OrderItem
 {
     public decimal Discount { get; set; }
     public Product? Product { get; set; }
-    public decimal TotalPrice { get; set; }
+    public decimal TotalPrice
+    {
+        get
+        {
+            return (Product != null) ? Product.Price * Quantity - Discount : 0;
+        }
+    }
     public int Quantity { get; set; }
 
-    public override string ToString() => $"{Product} {Quantity} {TotalPrice}";
+    public override string ToString() => $"{Product} {Quantity} {Discount} {TotalPrice}";
 }
--- Grundkurs/WestcoastEShop-FirstTry/Program.cs
+++ Grundkurs/WestcoastEShop-FirstTry/Program.cs
@@ -2,6 +2,33 @@
 using System.Text.Json;
 using System.Text.Encodings.Web;
 
+var product1 = new Product
+{
+    ProductId = 1,
+    ProductName = "Crowbar",
+    Price = 400M
+};
+
+var product2 = new Product
+{
+    ProductId = 2,
+    ProductName = "Hammer",
+    Price = 200M
+};
+
+var orderItem1 = new OrderItem
+{
+    Product = product1,
+    Quantity = 3,
+    Discount = 10M
+};
+
+var orderItem2 = new OrderItem
+{
+    Product = product2,
+    Quantity = 2
+};
+
 var salesOrder = new SalesOrder
 {
     OrderDate = DateTime.Now,
@@ -16,7 +43,8 @@ var salesOrder = new SalesOrder
         AddressLine = "Storgatan 8",
         PostalCode = "123 45",
         City = "Storstad"
-    }
+    },
+    OrderItems = [orderItem1, orderItem2]
 };
 
 var jsonOptions = new JsonSerializerOptions
