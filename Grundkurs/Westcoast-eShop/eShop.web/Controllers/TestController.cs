3f31534c0dba1e2896365e068a79c1bc840829c9
CalinJilavu <23583666+c8j@users.noreply.github.com>
2024-10-10 12:54:17 +0200

    Test exercise

---
+++ Grundkurs/Westcoast-eShop/eShop.web/Controllers/TestController.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace eShop.web.Controllers;
+
+public class TestController : Controller
+{
+    // GET: TestController
+    public ActionResult Index()
+    {
+        return View();
+    }
+
+}
