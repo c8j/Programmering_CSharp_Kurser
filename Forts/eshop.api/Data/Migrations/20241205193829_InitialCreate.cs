4695c333125f8b7dec397efcffd8da1e76d9e8ad
CalinJilavu <23583666+c8j@users.noreply.github.com>
2024-12-09 19:13:22 +0100

    Create database from code

---
+++ Forts/eshop.api/Data/Migrations/20241205193829_InitialCreate.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace eshop.api.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    ItemNumber = table.Column<string>(type: "TEXT", nullable: true),
+                    ProductName = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.ID);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Products");
+        }
+    }
+}
--- Forts/eshop.api/Entities/Product.cs
+++ Forts/eshop.api/Entities/Product.cs
@@ -5,4 +5,5 @@ public class Product
     public int ID { get; set; }
     public string ItemNumber { get; set; }
     public string ProductName { get; set; }
+    public decimal Price { get; set; }
 }
